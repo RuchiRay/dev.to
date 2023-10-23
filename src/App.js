0e038ea41ddda48cb335d0eaf768c2ac25eea393
RuchiRay <ruchiray18@gmail.com>
2023-10-23 20:09:35 +0530

    added splash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect ,useState} from "react";
+import { Splash } from "./components/Splash";
+
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true)
+  useEffect(() => {
+   setTimeout(() => {
+    setShowSplash(false)
+   }, 5000);
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {showSplash &&<Splash/>}
     </div>
   );
 }
